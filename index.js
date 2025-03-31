71b979a29fec12500476d93034d1292f2d1eb242
SAPNathasha <131260315+SAPNathasha@users.noreply.github.com>
2025-03-31 09:56:17 +0530

    Update the code to jQuery

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 function calculateLoan() {
-    loanAmountValue = document.getElementById("loan-amount").value;
-    interestRateValue = document.getElementById("interest-rate").value;
-    MonthsToPayValue = document.getElementById("months-to-pay").value;
-  
-    interest = (loanAmountValue * (interestRateValue * 0.01)) / MonthsToPayValue;
-  
-    monthlyPayment = (loanAmountValue / MonthsToPayValue + interest).toFixed(2);
-  
-    document.getElementById(
-      "payment"
-    ).innerHTML = `Monthly Payment: ${monthlyPayment}`;
-  }
+    let loanAmountValue = $("#loan-amount").val();
+    let interestRateValue = $("#interest-rate").val();
+    let MonthsToPayValue = $("#months-to-pay").val();
+
+    let interest = (loanAmountValue * (interestRateValue * 0.01)) / MonthsToPayValue;
+
+    let monthlyPayment = (loanAmountValue / MonthsToPayValue + interest).toFixed(2);
+
+    $("#payment").html(`Monthly Payment: ${monthlyPayment}`);
+}
